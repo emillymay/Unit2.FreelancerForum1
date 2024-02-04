1ec4f940fc343cf6a711402fb13310b4123fa9ab
emily <emillymay@LAPTOP-FPN7UJU6>
2024-02-04 13:45:11 -0700

    created freelance table

--- index.js
+++ index.js
@@ -4,47 +4,6 @@ const freelancers = [
     {name: 'Bob', occupation: 'Teacher', startingPrice: 50},
     {name: 'Carol', occupation: 'Programmer', startingPrice: 70}
     ];
-    //create function that calculates starting price
-    function calculateAveragePrice() {
-        const totalPrice = freelancers.reduce((sum, freelancer)=> sum + startingPrice, 0);
-        const averagePrice = totalPrice / freelancers.length || 0;
-        return averagePrice.toFixed(2); // Limit to two decimals
-    }
-    //function to update html with current list of freelancers
-    function updateFreelancersList() {
-        const freelancerListElement = document.getElementById('freelancer-list');
-        const averagePriceElement = document.getElementById('average-price');
-    }
+    //create function that calculates starting price average
+    const priceValue = document.querySelector("#priceValue");
     
-    //clear the current list
-     freelancerListElement.innerHTML = '';
-    
-     //create new list with each freelancer
-     freelancers.forEach(freelancer => {
-         const listItem = document.createElement('li');
-         listItem.textContent = `${freelancer.name} - ${freelancer.occupation}, Starting Price: $${freelancer.startingPrice}`;
-         freelancerListElement.appendChild(listItem);
-     });
-    
-     // Update the sum price
-     const averagePrice = calculateAveragePrice();
-     averagePriceElement.textContent = `Average starting price: $${averagePrice}`;
-    
-    
-    //create function to add new freelancers
-    function addFreelancer(name, occupation, startingPrice) {
-     const newFreelancer = { name, occupation, startingPrice };
-     freelancers.push(newFreelancer);
-     updateFreelancerList();
-    }
-    
-    //use setInterval to allow new freelancers to appear every few seconds
-    setInterval(() => {
-     const newFreelancerName = prompt('Enter name of the freelancer:');
-     const newFreelancerOccupation = prompt('Enter occupation:');
-     const newFreelancerStartingPrice = parseFloat(prompt('Enter the starting price:'));
-     
-     addFreelancer(newFreelancerName, newFreelancerOccupation, newFreelancerStartingPrice);
-    }, 5000); //update freelancer list every 5 seconds
-    
-    updateFreelancerList();
